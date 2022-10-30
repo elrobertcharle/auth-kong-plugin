b2c918d8fea6340342eb3f83a245d5ab70dc4016
Lola Flores <lolafloresrcc@gmail.com>
2022-10-30 04:54:37 -0400

    misc

--- install-plugin.ps1
+++ install-plugin.ps1
@@ -1,4 +1,8 @@
 $ContainerId = Read-Host -Prompt 'Input ContainerId'
+docker exec --user=root ${ContainerId} apt-get update
+docker exec --user=root ${ContainerId} apt-get install zip
+docker exec --user=root ${ContainerId} apt-get install unzip
+docker exec --user=root ${ContainerId} luarocks install lua-resty-http
 docker exec --user=root ${ContainerId} rm /del/asheauth-1.0-3.all.rock
 docker exec --user=root ${ContainerId} rm /del/asheauth-1.0-3.rockspec
 docker exec --user=root ${ContainerId} rm -r /del/kong
@@ -6,7 +10,9 @@ docker exec --user=root ${ContainerId} mkdir -p /del/kong/plugins/asheauth
 docker cp "D:\Documents\Ashe\KongAuthPlugin\kong\plugins\asheauth\handler.lua" ${ContainerId}:/del/kong/plugins/asheauth/handler.lua
 docker cp "D:\Documents\Ashe\KongAuthPlugin\kong\plugins\asheauth\schema.lua" ${ContainerId}:/del/kong/plugins/asheauth/schema.lua
 docker cp "D:\Documents\Ashe\KongAuthPlugin\kong-plugin-asheauth-1.0-3.rockspec" ${ContainerId}:/del/asheauth-1.0-3.rockspec
+docker exec  --user=root ${ContainerId} /bin/bash -c  echo "luarocks make"
 docker exec  --user=root ${ContainerId} /bin/bash -c "cd /del && luarocks make"
+docker exec  --user=root ${ContainerId} /bin/bash -c  echo "luarocks pack"
 docker exec  --user=root ${ContainerId} /bin/bash -c "cd /del && luarocks pack asheauth 1.0-3"
 docker exec  --user=root ${ContainerId} /bin/bash -c "cd /del && luarocks install asheauth-1.0-3.all.rock"
 docker exec --user root ${ContainerId} rm /etc/kong/kong.conf.default
--- kong/plugins/asheauth/handler.lua
+++ kong/plugins/asheauth/handler.lua
@@ -10,11 +10,27 @@ function AsheauthHandler:access(conf)
 end
 
 function AsheauthHandler:header_filter(conf)   
-  kong.log.debug("response ok XXXX")   
+  kong.log.debug("response ok YYYY")     
+end
+
+function AsheauthHandler:rewrite(conf)   
+  kong.log.debug("enter rewrite")   
 
   local httpc = http:new()
 
-  kong.response.set_header("ENTRO", "ok") 
+  local res, err = httpc:request_uri("https://www.microsoft.com/en-us", {
+    method = "GET"    
+  })
+  
+  if not res then
+    kong.log.debug("res is nil")
+    return
+  else
+    local response_body = res:read_body()
+    kong.log.debug("status: " .. res.status .. "Body: " .. response_body)  
+  end
+
+  kong.log.debug("rewrite OK")   
 end
 
 
