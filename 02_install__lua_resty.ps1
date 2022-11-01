e2c08537d11e66a0cab8ddc6ea0d0d0e55ab7278
Lola Flores <lolafloresrcc@gmail.com>
2022-11-01 00:18:06 -0400

    misc

--- 02_install__lua_resty.ps1
+++ 02_install__lua_resty.ps1
@@ -1,7 +1,21 @@
-$ContainerId = Read-Host -Prompt 'Input ContainerId'
-docker exec --user=root -it 8a03a3693e9cdbb71544c0e9543346d8ea38e3a335f3547f06a454d1041f993c /bin/bash
-
+Write-Host @"
+Execute these:
 apt-get update
+apt-get install libssl-dev
 yes |apt-get install zip
 yes | apt-get install unzip
-luarocks install lua-resty-http
+yes | apt-get install gcc
+apt-get install make
+yes | apt-get install build-essential
+luarocks install luaossl
+yes | apt-get install m4
+luarocks install cqueues
+luarocks install fifo
+luarocks install basexx
+luarocks install http
+"@
+
+$ContainerId = Read-Host -Prompt 'Input ContainerId'
+docker exec --user=root ${ContainerId} mkdir -p /del
+docker cp "D:\Documents\Ashe\KongAuthPlugin\install_lua-http.sh" ${ContainerId}:/del/install_lua-http.sh
+docker exec --user=root -it ${ContainerId} /bin/bash -c "cd /del && install_lua-http.sh"
---
+++ install_lua-http.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#apt-get install <package name> -y
+apt-get update
+apt-get install libssl-dev
+apt-get install zip -y
+apt-get install unzip -y
+apt-get install gcc -y
+apt-get install make
+apt-get install build-essential -y
+luarocks install luaossl
+apt-get install m4 -y
+luarocks install cqueues
+luarocks install fifo
+luarocks install basexx
+luarocks install http
