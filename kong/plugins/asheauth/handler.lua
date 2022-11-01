613e88e8862d9a1e122e35caba6b39994952cb8b
Lola Flores <lolafloresrcc@gmail.com>
2022-11-01 05:24:46 -0400

    misc

--- kong/plugins/asheauth/handler.lua
+++ kong/plugins/asheauth/handler.lua
@@ -3,22 +3,26 @@ local AsheauthHandler = {
   VERSION = "1.0"
 }
 
-function AsheauthHandler:access(conf)
-   
+local function reques_http()
+  local request = require "http.request"
+  local new_request= request.new_from_uri("http://172.22.48.1:5075/api/test/foo") 
+  local res_headers, res_stream = new_request:go(100);
+  if res_headers then
+   for lua_field, lua_value in res_headers:each()
+    do
+     kong.log.debug("field: " .. lua_field .. " value: " .. lua_value)
+    end
+  end
+  local res_body = assert(res_stream:get_body_as_string())
+  kong.log.debug("body:" .. res_body)
 end
 
-function AsheauthHandler:header_filter(conf)   
-   
-end
-
-function AsheauthHandler:rewrite(conf)   
-  kong.log.debug("enter rewrite")   
-
+local function resquest_resty_http()
   local httpc = require("resty.http").new()
 
   local ok, err = httpc:connect({
     scheme = "http",
-    host = "127.0.0.1",
+    host = "172.22.48.1",
     port = 5075,
     pool = "kongpool",
     pool_size = 20
@@ -34,7 +38,7 @@ function AsheauthHandler:rewrite(conf)
     method = "GET",
     path = "/api/test/foo",
     headers = {
-      ["Host"] = "localhost:5075",
+      ["Host"] = "172.22.48.1:5075",
     },
   })
 
@@ -47,8 +51,23 @@ function AsheauthHandler:rewrite(conf)
   else
     local response_body = res:read_body()
     local status = res.status
-    kong.log.debug("read body status: " .. status .. " body: " .. body)
+    kong.log.debug("read body status: " .. status .. " body: " .. response_body)
   end
+end
+
+function AsheauthHandler:access(conf)
+   
+end
+
+function AsheauthHandler:header_filter(conf)   
+   
+end
+
+function AsheauthHandler:rewrite(conf)   
+  kong.log.debug("enter rewrite")   
+
+  resquest_resty_http()
+  reques_http()
 
   kong.log.debug("rewrite OK")   
 end
