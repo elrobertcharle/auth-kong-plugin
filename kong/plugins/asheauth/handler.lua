6f6559a52ed04470c572891dbff167897e7c63dd
Lola Flores <lolafloresrcc@gmail.com>
2022-11-02 03:41:56 -0400

    perfect

--- kong/plugins/asheauth/handler.lua
+++ kong/plugins/asheauth/handler.lua
@@ -5,27 +5,10 @@ local AsheauthHandler = {
   VERSION = "1.0"
 }
 
-local function reques_http(auth_url, st)
-  local request = require "http.request"
-  local new_request= request.new_from_uri(auth_url) 
-  local res_headers, res_stream = new_request:go(100);
-  if res_headers then
-   for lua_field, lua_value in res_headers:each()
-    do
-     kong.log.debug("field: " .. lua_field .. " value: " .. lua_value)
-    end
-  end
-  local res_body = assert(res_stream:get_body_as_string())
-  kong.log.debug("body:" .. res_body)
-end
-
-
-
 local function resquest_resty_http(auth_url, st)
   local httpc = http:new()
-  local res, req_err = httpc:request_uri("http://localhost:5075/api/auth/validate-st", {
+  local res, req_err = httpc:request_uri(auth_url, {
     method = "GET",
-    ssl_verify = false,
     headers = {["st"] = st}
   })
 
@@ -36,60 +19,17 @@ local function resquest_resty_http(auth_url, st)
 
   if res then
     local status = res.status
-    local response_body = res:read_body()
-    if not res_body then
-      res_body=""      
-    end
-    kong.log.debug("read body status: " .. status .. " body: " .. response_body)
+    --local res_body = res:read_body()
+    --if not res_body then
+    --  res_body=""      
+    --end
+    --kong.log.debug("read body status: " .. status .. " body: " .. res_body)
     return status   
   else
     kong.log.debug("res is nil")
     return 500
   end
 end
-
-local function resquest_resty_http2(st)
-  local httpc = require("resty.http").new()
-
-  local ok, err = httpc:connect({
-    scheme = "http",
-    host = "172.22.48.1",
-    port = 5075,
-    })
-  if ok then
-    kong.log.debug("connected ok")
-  else
-    kong.log.debug("connect err: " .. err)
-    return 500
-  end
-
-  local res, req_err = httpc:request({
-    method = "GET",
-    path = "/api/auth/validate-st",
-    headers = {
-      ["st"] = st,
-    },
-  })
-
-  if req_err then
-    kong.log.debug("req_err: " .. req_err)
-    return 500
-  end
-
-  if res then
-    local status = res.status
-    local response_body = res:read_body()
-    if not res_body then
-      res_body=""      
-    end
-    kong.log.debug("read body status: " .. status .. " body: " .. response_body)
-    return status   
-  else
-    kong.log.debug("res is nil")
-    return 500
-  end
-end
-
 
 function AsheauthHandler:access(conf)
   kong.log.debug("enter access")
@@ -114,8 +54,7 @@ function AsheauthHandler:access(conf)
     kong.log.debug("st: " .. st)
   end
 
- --local status= resquest_resty_http(conf.authorization_url, st)
- local status= resquest_resty_http2(st)
+ local status= resquest_resty_http(conf.authorization_url, st)
  if status~=200 then
   kong.response.exit(status)
  end
