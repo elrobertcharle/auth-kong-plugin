2f9ca598f44bec41d13b05587d6bb9847a24b5e7
Lola Flores <lolafloresrcc@gmail.com>
2022-11-02 03:14:20 -0400

    working

--- kong/plugins/asheauth/handler.lua
+++ kong/plugins/asheauth/handler.lua
@@ -5,9 +5,9 @@ local AsheauthHandler = {
   VERSION = "1.0"
 }
 
-local function reques_http()
+local function reques_http(auth_url, st)
   local request = require "http.request"
-  local new_request= request.new_from_uri("http://172.22.48.1:5075/api/test/foo") 
+  local new_request= request.new_from_uri(auth_url) 
   local res_headers, res_stream = new_request:go(100);
   if res_headers then
    for lua_field, lua_value in res_headers:each()
@@ -19,9 +19,11 @@ local function reques_http()
   kong.log.debug("body:" .. res_body)
 end
 
+
+
 local function resquest_resty_http(auth_url, st)
   local httpc = http:new()
-  local res, req_err = httpc:request_uri(auth_url, {
+  local res, req_err = httpc:request_uri("http://localhost:5075/api/auth/validate-st", {
     method = "GET",
     ssl_verify = false,
     headers = {["st"] = st}
@@ -46,6 +48,50 @@ local function resquest_resty_http(auth_url, st)
   end
 end
 
+local function resquest_resty_http2(st)
+  local httpc = require("resty.http").new()
+
+  local ok, err = httpc:connect({
+    scheme = "http",
+    host = "172.22.48.1",
+    port = 5075,
+    })
+  if ok then
+    kong.log.debug("connected ok")
+  else
+    kong.log.debug("connect err: " .. err)
+    return 500
+  end
+
+  local res, req_err = httpc:request({
+    method = "GET",
+    path = "/api/auth/validate-st",
+    headers = {
+      ["Host"] = "172.22.48.1:5075",
+      ["st"] = st,
+    },
+  })
+
+  if req_err then
+    kong.log.debug("req_err: " .. req_err)
+    return 500
+  end
+
+  if res then
+    local status = res.status
+    local response_body = res:read_body()
+    if not res_body then
+      res_body=""      
+    end
+    kong.log.debug("read body status: " .. status .. " body: " .. response_body)
+    return status   
+  else
+    kong.log.debug("res is nil")
+    return 500
+  end
+end
+
+
 function AsheauthHandler:access(conf)
   kong.log.debug("enter access")
   kong.log.debug("authorization_url: " .. conf.authorization_url)
@@ -64,10 +110,13 @@ function AsheauthHandler:access(conf)
  
   --local st = kong.request.get_headers()["st"]
   if not st then
-      kong.response.exit(401)
+    kong.response.exit(401)
+  else
+    kong.log.debug("st: " .. st)
   end
 
- local status= resquest_resty_http(conf.authorization_url, st)
+ --local status= resquest_resty_http(conf.authorization_url, st)
+ local status= resquest_resty_http2(st)
  if status~=200 then
   kong.response.exit(status)
  end
