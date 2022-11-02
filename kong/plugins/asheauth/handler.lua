ea08638ce989b8e1473c72384db110f950ea272c
Lola Flores <lolafloresrcc@gmail.com>
2022-11-01 22:17:08 -0400

    beta

--- kong/plugins/asheauth/handler.lua
+++ kong/plugins/asheauth/handler.lua
@@ -17,46 +17,64 @@ local function reques_http()
   kong.log.debug("body:" .. res_body)
 end
 
-local function resquest_resty_http()
+local function resquest_resty_http(st)
   local httpc = require("resty.http").new()
 
   local ok, err = httpc:connect({
     scheme = "http",
     host = "172.22.48.1",
     port = 5075,
-    pool = "kongpool",
-    pool_size = 20
     })
   if ok then
     kong.log.debug("connected ok")
   else
     kong.log.debug("connect err: " .. err)
-    return
+    return 500
   end
 
-  local res, err1 = httpc:request({
+  local res, req_err = httpc:request({
     method = "GET",
-    path = "/api/test/foo",
+    path = "/api/auth/validate-st",
     headers = {
       ["Host"] = "172.22.48.1:5075",
+      ["st"] = st,
     },
   })
 
-  if err1 then
-    kong.log.debug("err: " .. err1)
+  if req_err then
+    kong.log.debug("req_err: " .. req_err)
+    return 500
   end
 
-  if not res then
-    kong.log.debug("res is nil")
-  else
-    local response_body = res:read_body()
+  if res then
     local status = res.status
+    local response_body = res:read_body()
+    if not res_body then
+      res_body=""      
+    end
     kong.log.debug("read body status: " .. status .. " body: " .. response_body)
+    return status   
+  else
+    kong.log.debug("res is nil")
+    return 500
   end
 end
 
 function AsheauthHandler:access(conf)
-   
+  kong.log.debug("enter access")   
+  
+  local st = kong.request.get_headers()["st"]
+  if not st then
+      kong.response.exit(401)
+  end
+
+ local status= resquest_resty_http(st)
+ if status~=200 then
+  kong.response.exit(status)
+ end
+  -- reques_http()
+
+  kong.log.debug("access passed OK")    
 end
 
 function AsheauthHandler:header_filter(conf)   
@@ -64,12 +82,7 @@ function AsheauthHandler:header_filter(conf)
 end
 
 function AsheauthHandler:rewrite(conf)   
-  kong.log.debug("enter rewrite")   
-
-  resquest_resty_http()
-  reques_http()
-
-  kong.log.debug("rewrite OK")   
+ 
 end
 
 
